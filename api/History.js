0280af0292db79be65088ca31ef2a33c643adc03
Rulai Hu <hurulaih@sfu.ca>
2020-03-15 07:09:04 -0700

    docs

--- api/History.js
+++ api/History.js
@@ -1,11 +1,23 @@
 class History {
+    /**
+     * History of games.
+     * @type {Array}
+     */
     history = [];
 
+    /**
+     * Add a game to the history.
+     * @param {game} game
+     */
     add(game) {
         game.date = Date.now();
         this.history.push(game);
     }
 
+    /**
+     * Return the entire history of games.
+     * @return {[Object]}
+     */
     all() {
         return this.history.map(game => ({
             date: game.date,
@@ -14,6 +26,10 @@ class History {
         }));
     }
 
+    /**
+     * Return the win/lose amounts for X and O.
+     * @return {Object}
+     */
     summarize() {
         return this.history.reduce(function(sum, game) {
             if (game.winner == 'X') {
